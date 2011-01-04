eb749f4d7de18efc1f2416f2787cb929839a54a2
Jacobo García <jacobo.garcia@gmail.com>
2011-01-04 15:40:17 +0100

    Fix: removing unused dependancies

--- cookbooks/apache2/metadata.rb
+++ cookbooks/apache2/metadata.rb
@@ -41,9 +41,6 @@ recipe            "apache2::mod_setenvif", "Apache module 'setenvif' with config
 recipe            "apache2::mod_ssl", "Apache module 'ssl' with config file, adds port 443 to listen_ports"
 recipe            "apache2::mod_status", "Apache module 'status' with config file"
 
-depends 'ree'
-depends  'apt'
-
 %w{redhat centos debian ubuntu}.each do |os|
   supports os
 end
