0c15d955b6faaf74a9daa4bb2e2da6960c237dc2
Jacobo García <jacobo.garcia@gmail.com>
2011-02-24 01:35:02 +0100

    Fix: make sure that we relaunch apt-get update after a change in the repository.list file even if we already have the repository key

--- cookbooks/apt/definitions/repository.rb
+++ cookbooks/apt/definitions/repository.rb
@@ -1,6 +1,6 @@
 define :repository do
 
-include_recipe "apt"
+  include_recipe "apt::default"
 
   directory "/etc/apt/sources.list.d/" do
     action :create
@@ -9,22 +9,46 @@ include_recipe "apt"
     mode "0755"
   end
 
-  remote_file "/etc/apt/sources.list.d/#{params[:name]}.list" do
-    params[:config_url] ? (source params[:config_url]) : (source params[:name])
-    owner "root"
-    group "root"
-    mode "0644"
-    action :create
-  end
+  if params[:name].include?("ppa:")
+    if params[:name].include?("+")
 
-  remote_file "/tmp/#{params[:name]}-apt.key" do
-    params[:key_url] ?  (source params[:key_url])  : (source "#{params[:name]}.key")
-  end
+      sources_original=params[:name].gsub("ppa:","").gsub("/","-")
+      sources_list=params[:name].gsub("+","-").gsub("ppa:","").gsub("/","-")
 
-  execute "apt-key add /tmp/#{params[:name]}-apt.key" do
-    creates "/var/tmp/.add-key-#{params[:name]}"
-    notifies :run, resources(:execute => "apt-get update"), :immediately
-  end
+      execute "sudo add-apt-repository #{params[:name]}" do
+        creates "/etc/apt/sources.list.d/#{sources_list}-#{node[:lsb][:codename]}.list"
+      end
 
-  file "/var/tmp/.add-key-#{params[:name]}"
+      execute "mv /etc/apt/sources.list.d/#{sources_original}-#{node[:lsb][:codename]}.list /etc/apt/sources.list.d/#{sources_list}-#{node[:lsb][:codename]}.list" do
+        creates "/etc/apt/sources.list.d/#{sources_list}-#{node[:lsb][:codename]}.list"
+        notifies :run, resources(:execute => "apt-get update"), :immediately
+      end
+    else
+
+      execute "sudo add-apt-repository #{params[:name]}" do
+        creates "/etc/apt/sources.list.d/#{sources_list}-#{node[:lsb][:codename]}.list"
+        notifies :run, resources(:execute => "apt-get update"), :immediately
+      end
+    end
+  else
+
+    remote_file "/tmp/#{params[:name]}-apt.key" do
+      params[:key_url] ?  (source params[:key_url])  : (source "#{params[:name]}.key")
+    end
+
+    execute "apt-key add /tmp/#{params[:name]}-apt.key" do
+      creates "/var/tmp/.add-key-#{params[:name]}"
+    end
+
+    file "/var/tmp/.add-key-#{params[:name]}"
+
+    remote_file "/etc/apt/sources.list.d/#{params[:name]}.list" do
+      params[:config_url] ? (source params[:config_url]) : (source params[:name])
+      owner "root"
+      group "root"
+      mode "0644"
+      action :create
+      notifies :run, resources(:execute => "apt-get update"), :immediately
+    end
+  end
 end
