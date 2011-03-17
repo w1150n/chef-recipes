13f9450276393fb6820b820acf8c384dde704369
Jacobo García <jacobo.garcia@gmail.com>
2011-03-17 18:22:31 +0100

    Fix: this workaround only works now for the affected chef versions

--- cookbooks/gems/libraries/gem_patch.rb
+++ cookbooks/gems/libraries/gem_patch.rb
@@ -16,124 +16,128 @@
 # limitations under the License.
 #
 
-require 'chef/provider/package'
-require 'chef/mixin/command'
-require 'chef/resource/package'
+if Chef::VERSION > "0.8.10" && Chef::VERSION < "0.9.0"
 
-class Chef
-  class Provider
-    class Package
-      class Rubygems < Chef::Provider::Package
+  require 'chef/provider/package'
+  require 'chef/mixin/command'
+  require 'chef/resource/package'
 
-        def gem_list_parse(line)
-          installed_versions = Array.new
-          if version_and_platforms_match_data = line.match(/^#{@new_resource.package_name} \((.+?)\)$/)
-            version_and_platforms_string = version_and_platforms_match_data.captures.first
-            versions_and_platforms = version_and_platforms_string.split(/, /)
-            versions_and_platforms.map do |version_and_platforms|
-              version = version_and_platforms.split(' ').first
-              version
-            end
-          else
-            nil
-          end
-        end
+  class Chef
+    class Provider
+      class Package
+        class Rubygems < Chef::Provider::Package
 
-        def gem_binary_path
-          path = @new_resource.gem_binary
-          path ? path : 'gem'
-        end
 
-        def load_current_resource
-          @current_resource = Chef::Resource::Package.new(@new_resource.name)
-          @current_resource.package_name(@new_resource.package_name)
-          @current_resource.version(nil)
 
-          # First, we need to look up whether we have the local gem installed or not
-          status = popen4("#{gem_binary_path} list --local #{@new_resource.package_name}") do |pid, stdin, stdout, stderr|
-            stdout.each_line do |line|
-              next unless installed_versions = gem_list_parse(line)
-              # If the version we are asking for is installed, make that our current
-              # version.  Otherwise, go ahead and use the highest one, which
-              # happens to come first in the array.
-              if installed_versions.detect { |v| v == @new_resource.version }
-                Chef::Log.debug("#{@new_resource.package_name} at version #{@new_resource.version}")
-                @current_resource.version(@new_resource.version)
-              else
-                iv = installed_versions.first
-                Chef::Log.debug("#{@new_resource.package_name} at version #{iv}")
-                @current_resource.version(iv)
+          def gem_list_parse(line)
+            installed_versions = Array.new
+            if version_and_platforms_match_data = line.match(/^#{@new_resource.package_name} \((.+?)\)$/)
+              version_and_platforms_string = version_and_platforms_match_data.captures.first
+              versions_and_platforms = version_and_platforms_string.split(/, /)
+              versions_and_platforms.map do |version_and_platforms|
+                version = version_and_platforms.split(' ').first
+                version
               end
+            else
+              nil
             end
           end
 
-          unless status.exitstatus == 0
-            raise Chef::Exceptions::Package, "#{gem_binary_path} list --local failed - #{status.inspect}!"
+          def gem_binary_path
+            path = @new_resource.gem_binary
+            path ? path : 'gem'
           end
 
-          @current_resource
-        end
+          def load_current_resource
+            @current_resource = Chef::Resource::Package.new(@new_resource.name)
+            @current_resource.package_name(@new_resource.package_name)
+            @current_resource.version(nil)
 
-        def candidate_version
-          return @candidate_version if @candidate_version
-
-          status = popen4("#{gem_binary_path} list --remote #{@new_resource.package_name}#{' --source=' + @new_resource.source if @new_resource.source}") do |pid, stdin, stdout, stderr|
-            stdout.each_line do |line|
-              next unless installed_versions = gem_list_parse(line)
-              Chef::Log.debug("candidate_version: remote rubygem(s) available: #{installed_versions.inspect}")
-
-              unless installed_versions.empty?
-                Chef::Log.debug("candidate_version: setting install candidate version to #{installed_versions.first}")
-                @candidate_version = installed_versions.first
+            # First, we need to look up whether we have the local gem installed or not
+            status = popen4("#{gem_binary_path} list --local #{@new_resource.package_name}") do |pid, stdin, stdout, stderr|
+              stdout.each_line do |line|
+                next unless installed_versions = gem_list_parse(line)
+                # If the version we are asking for is installed, make that our current
+                # version.  Otherwise, go ahead and use the highest one, which
+                # happens to come first in the array.
+                if installed_versions.detect { |v| v == @new_resource.version }
+                  Chef::Log.debug("#{@new_resource.package_name} at version #{@new_resource.version}")
+                  @current_resource.version(@new_resource.version)
+                else
+                  iv = installed_versions.first
+                  Chef::Log.debug("#{@new_resource.package_name} at version #{iv}")
+                  @current_resource.version(iv)
+                end
               end
             end
 
+            unless status.exitstatus == 0
+              raise Chef::Exceptions::Package, "#{gem_binary_path} list --local failed - #{status.inspect}!"
+            end
+
+            @current_resource
           end
 
-          unless status.exitstatus == 0
-            raise Chef::Exceptions::Package, "#{gem_binary_path} list --remote failed - #{status.inspect}!"
+          def candidate_version
+            return @candidate_version if @candidate_version
+
+            status = popen4("#{gem_binary_path} list --remote #{@new_resource.package_name}#{' --source=' + @new_resource.source if @new_resource.source}") do |pid, stdin, stdout, stderr|
+              stdout.each_line do |line|
+                next unless installed_versions = gem_list_parse(line)
+                Chef::Log.debug("candidate_version: remote rubygem(s) available: #{installed_versions.inspect}")
+
+                unless installed_versions.empty?
+                  Chef::Log.debug("candidate_version: setting install candidate version to #{installed_versions.first}")
+                  @candidate_version = installed_versions.first
+                end
+              end
+
+            end
+
+            unless status.exitstatus == 0
+              raise Chef::Exceptions::Package, "#{gem_binary_path} list --remote failed - #{status.inspect}!"
+            end
+            @candidate_version
           end
-          @candidate_version
-        end
 
-        def install_package(name, version)
-          src = nil
-          if @new_resource.source
-            src = "  --source=#{@new_resource.source} --source=http://rubygems.org"
-          end
-          run_command_with_systems_locale(
-            :command => "#{gem_binary_path} install #{name} -q --no-rdoc --no-ri -v \"#{version}\"#{src}#{opts}"
-          )
-        end
-
-        def upgrade_package(name, version)
-          install_package(name, version)
-        end
-
-        def remove_package(name, version)
-          if version
+          def install_package(name, version)
+            src = nil
+            if @new_resource.source
+              src = "  --source=#{@new_resource.source} --source=http://rubygems.org"
+            end
             run_command_with_systems_locale(
-              :command => "#{gem_binary_path} uninstall #{name} -q -v \"#{version}\""
-            )
-          else
-            run_command_with_systems_locale(
-              :command => "#{gem_binary_path} uninstall #{name} -q -a"
-            )
+                                            :command => "#{gem_binary_path} install #{name} -q --no-rdoc --no-ri -v \"#{version}\"#{src}#{opts}"
+                                            )
           end
+
+          def upgrade_package(name, version)
+            install_package(name, version)
+          end
+
+          def remove_package(name, version)
+            if version
+              run_command_with_systems_locale(
+                                              :command => "#{gem_binary_path} uninstall #{name} -q -v \"#{version}\""
+                                              )
+            else
+              run_command_with_systems_locale(
+                                              :command => "#{gem_binary_path} uninstall #{name} -q -a"
+                                              )
+            end
+          end
+
+          def purge_package(name, version)
+            remove_package(name, version)
+          end
+
+          private
+
+          def opts
+            expand_options(@new_resource.options)
+          end
+
         end
-
-        def purge_package(name, version)
-          remove_package(name, version)
-        end
-
-        private
-
-        def opts
-          expand_options(@new_resource.options)
-        end
-
       end
     end
   end
 end
-
